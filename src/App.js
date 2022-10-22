7412178e6a4c3bcd556708cae19d2bc40252c78c
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-22 13:01:09 +0530

    Adding NotFound page for invalid url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AllQuotes from './components/pages/AllQuotes';
 import QuoteDetail from './components/pages/QuoteDetail';
 import NewQuote from './components/pages/NewQuote';
 import Layout from './components/layout/Layout';
+import { NotFound } from './components/pages/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/quotes" element={<AllQuotes />}/>
         <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}/>
         <Route path="/new-quote" element={<NewQuote />}/>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </Layout>
   );
