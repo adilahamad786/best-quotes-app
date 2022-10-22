e8b86d987e54e9da6b626fe147d7e70e634bdd06
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-22 12:27:18 +0530

    Adding Dummy_Quotes of list.

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import QuoteList from '../quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    { id : 'p1', author : "Adil Ahamad", text : "Time is very less!"},
+    { id : 'p2', author : "Adil Ahamad", text : "Mast raho suwast raho!"}
+];
 
 export const AllQuotes = () => {
   return (
-    <div>AllQuotes</div>
+    <QuoteList quotes={ DUMMY_QUOTES } />
   );
 };
 
