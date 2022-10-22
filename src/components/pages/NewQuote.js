974254ac9f5863ad7508c46d900d3f1e0e9475e3
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-22 13:48:47 +0530

    Redirect all quotes page after adding new quote.

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import QuoteForm from '../quotes/QuoteForm';
+import { useNavigate } from 'react-router-dom';
 
 export const NewQuote = () => {
 
+    const navigate = useNavigate();
+
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
+        navigate('/quotes');
     }
 
     return (
