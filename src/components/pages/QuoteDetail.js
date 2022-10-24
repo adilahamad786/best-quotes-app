03b4d4a400282fab6f3fc83c02aceb128975292a
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-24 17:19:46 +0530

    Fetching a single quote on details quote page.

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, Routes, Route, Link } from "react-router-dom";
 import Comments from "../comments/Comments";
 import HighlightedQuote from "../quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-  { id: "p1", author: "Adil Ahamad", text: "Time is very less!" },
-  { id: "p2", author: "Adil Ahamad", text: "Mast raho suwast raho!" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 export const QuoteDetail = () => {
-  const param = useParams();
+  const { quoteId } = useParams();
+  
+  const { sendRequest, status, data : loadedQuote, error } = useHttp(getSingleQuote, true);
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === param.quoteId);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
-  if (!quote) {
+  if (status === 'pending') {
+    return <div className='centered'>
+      <LoadingSpinner />
+    </div>
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>
+  }
+
+  if (!loadedQuote.text) {
     return <p>Quote not found!</p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Routes>
         <Route path="" element={
           <div className="centered">
